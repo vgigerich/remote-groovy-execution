7ec8cdef84c08155c92be51fc578b7479c0aee06
GitHub Action <action@github.com>
2022-03-02 17:06:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vgigerich/remotegroovyexecution/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vgigerich.remotegroovyexecution.services
+
+import com.github.vgigerich.remotegroovyexecution.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
